6b9a191542f4b32d20e076ceadcddd7176f517cb
aauuxyxy <u.u.a.a.x.y.x.y@kjb.biglobe.ne.jp>
2020-04-17 16:39:03 +0900

    URL遷移実装

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
 
 
 const routes: Routes = [
-    { path: '', component: ProductListComponent },
-    { path: 'detail', component: ProductDetailComponent },
+    { 
+        path: 'products', component: ProductComponent,
+        children:[
+            {path: '', component: ProductListComponent},
+            {path: 'detail', component: ProductDetailComponent},
+        ]
+    },
 ];
 
 @NgModule({
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,10 +3,12 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductComponent } from './product.component';
 
 
 @NgModule({
     declarations: [
+        ProductComponent,
         ProductListComponent,
         ProductDetailComponent,
     ],
