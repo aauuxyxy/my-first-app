a0c9613fc856e25546a2ceeb1159c58432464735
aauuxyxy <u.u.a.a.x.y.x.y@kjb.biglobe.ne.jp>
2020-04-18 18:09:05 +0900

    画像反映

--- src/app/product/products.ts
+++ src/app/product/products.ts
@@ -1,22 +1,50 @@
 export const products = [
     {
+        coverImage: './assets/img/iPhone.jpg',
         name: 'Phone XL',
         price: 799,
-        description: 'A large phone with one of the best screens'
+        description: 'A large phone with one of the best screens',
+        heading1: '魅了する光沢感',
+        heading2: '高いスペック',
+        heading3: '高精細な画質',
+        headingText1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText2: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
     },
     {
+        coverImage: './assets/img/iPhone2.jpg',
         name: 'Phone Mini',
         price: 699,
-        description: 'A great phone with one of the best cameras'
+        description: 'A great phone with one of the best cameras',
+        heading1: '魅了する光沢感',
+        heading2: '高いスペック',
+        heading3: '高精細な画質',
+        headingText1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText2: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
     },
     {
+        coverImage: './assets/img/iPhone3.webp',
         name: 'Phone Standard',
         price: 299,
-        description: 'A great phone with one of the best cameras'
+        description: 'A great phone with one of the best cameras',
+        heading1: '魅了する光沢感',
+        heading2: '高いスペック',
+        heading3: '高精細な画質',
+        headingText1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText2: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
     },
     {
+        coverImage: './assets/img/iPhone4.jpg',
         name: 'iPhone11',
         price: 1000,
-        description: 'レンズがキモいです。マジでどうにかしてください。'
+        description: 'レンズがキモいです。マジでどうにかしてください。',
+        heading1: '魅了する光沢感',
+        heading2: '高いスペック',
+        heading3: '高精細な画質',
+        headingText1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText2: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
+        headingText3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Praesent commodocursus magna.',
     }
 ];
